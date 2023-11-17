96095f571e8ca2c6b5b2d073fc2af8ced27ddde0
enixjm <cys08190819@gmail.com>
2023-11-17 18:36:30 +0900

    feature add markdown

--- src/main.js
+++ src/main.js
@@ -5,7 +5,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css';
 import router from '@/routes'
+import Markdown from 'vue3-markdown-it';
 
 createApp(App)
 .use(router)
+.use(Markdown)
 .mount('#app')
